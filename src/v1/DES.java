2430b6c15bc2ec734d406040965565589be3f673
Armand Marcelli <a.armar2003@gmail.com>
2022-11-05 20:47:01 +0100

    Fonction F faite !

--- src/v1/DES.java
+++ src/v1/DES.java
@@ -25,7 +25,7 @@ public class DES {
 	ArrayList<Integer> perm_initiale = new ArrayList<Integer>(List.of(58,50,42,34,26,18,10,2,60,52,44,36,28,20,12,4,62,54,46,38,30,22,14,6,64,56,48,40,32,24,16,8,57,49,41,33,25,17,9,1,59,51,43,35,27,19,11,3,61,53,45,37,29,21,13,5,63,55,47,39,31,23,15,7));
 	
 	int [] S = {14,4,13,1,2,15,11,8,3,10,6,12,5,9,0,7,0,15,7,4,14,2,13,1,10,6,12,11,9,5,3,8,4,1,14,8,13,6,2,11,15,12,9,7,3,10,5,0,15,12,8,2,4,9,1,7,5,11,3,14,10,0,6,13};
-	int [] E = {32,1,2,3,4,5,4,5,6,7,8,9,8,9,10,11,12,13,12,13,14,15,16,17,16,17,18,19,20,21,20,21,22,23,24,25,24,25,26,27,28,29,28,29,30,31,32,1};
+	ArrayList<Integer> E = new ArrayList<Integer>(List.of(32,1,2,3,4,5,4,5,6,7,8,9,8,9,10,11,12,13,12,13,14,15,16,17,16,17,18,19,20,21,20,21,22,23,24,25,24,25,26,27,28,29,28,29,30,31,32,1));
 	
 	//VARIABLES
 	ArrayList<Integer> masterKey;
@@ -244,6 +244,22 @@ public class DES {
 		}
 		
 		return newTab;
+	}
+	
+	public ArrayList<Integer> fonction_F (ArrayList<Integer> cle, ArrayList<Integer> D)
+	{
+		ArrayList<Integer> DnPrime = permutation(D, E);
+		ArrayList<Integer> DnEtoile = xor(DnPrime, cle);
+		ArrayList<ArrayList<Integer>> Ldn = decoupage(DnEtoile, 6);
+		ArrayList<ArrayList<Integer>> Sldn = new ArrayList<ArrayList<Integer>>();
+		
+		for(ArrayList<Integer> i : Ldn) {
+			Sldn.add(fonction_S(i));
+		}
+		
+		ArrayList<Integer> recollé = recollage_bloc(Sldn);
+		
+		return recollé;
 	}
 	
 	public void Crypte(String mess) {
